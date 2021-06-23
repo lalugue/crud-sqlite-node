3bd830b8312391a29f9ab6a80af4bdf3329a6890
lalugue <51893731+lalugue@users.noreply.github.com>
2021-06-23 22:44:03 +0800

    create "model"

---
+++ model.js
@@ -0,0 +1,12 @@
+const table = `CREATE TABLE IF NOT EXISTS todos (
+	id TEXT PRIMARY KEY,
+   	todo_description TEXT NOT NULL,
+	todo_responsible TEXT DEFAULT "NA",
+	todo_priority TEXT,
+    todo_completed INTEGER,
+    todo_deleted INTEGER DEFAULT 0
+)`;
+
+module.exports = {
+    table: table
+}
